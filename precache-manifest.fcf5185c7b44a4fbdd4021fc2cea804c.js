8c7d2cba6dbf64a7bcd691fb2eabd36902ea8e60
DmitriyZhukovv <d.zhukovv@gmail.com>
2019-07-12 11:52:22 +0300

    Updates

---
+++ precache-manifest.fcf5185c7b44a4fbdd4021fc2cea804c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdbf23f1cf18e3b244037d2a698a9c8",
+    "url": "/goit-fe-course/react/my-app/index.html"
+  },
+  {
+    "revision": "30e1488535edc4c0e897",
+    "url": "/goit-fe-course/react/my-app/static/css/main.08b7d24c.chunk.css"
+  },
+  {
+    "revision": "d38d423d10f0e0a710da",
+    "url": "/goit-fe-course/react/my-app/static/js/2.8661e072.chunk.js"
+  },
+  {
+    "revision": "30e1488535edc4c0e897",
+    "url": "/goit-fe-course/react/my-app/static/js/main.3b788e0e.chunk.js"
+  },
+  {
+    "revision": "0a1d951d781aa66fcae5",
+    "url": "/goit-fe-course/react/my-app/static/js/runtime~main.052b2cef.js"
+  }
+]);
